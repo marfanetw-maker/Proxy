713ef92676b85a14548506550b44a0927524cd81
Hossein Pira <158034265+code3-dev@users.noreply.github.com>
2025-08-19 00:35:21 +0330

    Update about_screen.dart

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -48,7 +48,7 @@ class AboutScreen extends StatelessWidget {
 
             // App Version
             const Text(
-              'Version 1.9.5',
+              'Version 2.0.0',
               style: TextStyle(fontSize: 16, color: Colors.grey),
             ),
 
